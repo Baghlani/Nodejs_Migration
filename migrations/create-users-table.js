b11627de36d778a8872eb3e5a966c5a768925d9c
Abolfazl Baghlani <afbaghlani@yahoo.com>
2019-10-15 14:41:12 +0330

    phase 1 completed

--- migrations/create-users-table.js
+++ migrations/create-users-table.js
@@ -14,14 +14,18 @@ let sch = Schema.create("users", table => {
 
     table.column("float").double(20, 10);
 
-
     table
         .column("tiny")
         .tinyInteger()
         .unsigned()
-        .comment('comment on tiny');
+        .comment("comment on tiny");
+
+    table
+        .primary("id")
+        .unique("tiny")
+        .charset("utf8mb4")
+        .collation("utf8mb4_persian_ci");
 });
 
-// console.log(sch.blueprint.structure.columns);
 
 module.exports = JSON.stringify(sch.blueprint);
